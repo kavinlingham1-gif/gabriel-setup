3ec96dfcaf82558e801445848e20a504978827c1
Kavin Lingham <kavin@sovereignatx.ai>
2026-04-10 13:54:54 -0500

    Fix: run tailscaled daemon in background, verify install

--- join-tailscale.sh
+++ join-tailscale.sh
@@ -7,9 +7,12 @@ echo "Installing Homebrew..."
 echo "Installing Tailscale..."
 /opt/homebrew/bin/brew install tailscale
 
+echo "Verifying Tailscale installation..."
+which tailscale
+
 echo "Starting Tailscale daemon..."
-sudo launchctl load /Library/LaunchDaemons/com.tailscale.ipn.plist
-sleep 2
+sudo /opt/homebrew/bin/tailscaled &
+sleep 3
 
 echo "Joining Tailscale network..."
 sudo /opt/homebrew/bin/tailscale up --authkey=tskey-auth-kEAMGg1yeJ11CNTRL-NNJQcH2h1aSzBCDLPWJ8ZSDP9PsxjUGac
