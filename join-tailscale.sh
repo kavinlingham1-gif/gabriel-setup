2bcb7f48c8316bead49bf93166cd5f7d8ab5880d
Kavin Lingham <kavin@sovereignatx.ai>
2026-04-10 13:52:14 -0500

    Fix: start tailscale daemon before connecting

--- join-tailscale.sh
+++ join-tailscale.sh
@@ -7,8 +7,13 @@ echo "Installing Homebrew..."
 echo "Installing Tailscale..."
 /opt/homebrew/bin/brew install tailscale
 
+echo "Starting Tailscale daemon..."
+sudo launchctl load /Library/LaunchDaemons/com.tailscale.ipn.plist
+sleep 2
+
 echo "Joining Tailscale network..."
 sudo /opt/homebrew/bin/tailscale up --authkey=tskey-auth-kEAMGg1yeJ11CNTRL-NNJQcH2h1aSzBCDLPWJ8ZSDP9PsxjUGac
 
+sleep 2
 echo "Done. You're on Tailscale."
 /opt/homebrew/bin/tailscale ip -4
